0ddb274408971468e703c1c1bc36e93697472dcb
Sanna Aho <Sanna.Aho@edu.savonia.fi>
2019-09-18 11:36:41 +0300

    Perusnäkymien pohjat

---
+++ app/src/main/java/com/example/ravitsemussovellus/KyselyActivity.java
@@ -0,0 +1,25 @@
+package com.example.ravitsemussovellus;
+
+import android.os.Bundle;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.view.View;
+
+public class KyselyActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_kysely);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+
+    }
+
+}
--- app/src/main/java/com/example/ravitsemussovellus/MainActivity.java
+++ app/src/main/java/com/example/ravitsemussovellus/MainActivity.java
@@ -2,13 +2,50 @@ package com.example.ravitsemussovellus;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import static android.provider.AlarmClock.EXTRA_MESSAGE;
 
 public class MainActivity extends AppCompatActivity {
+    private Button btnKysely, btnRaportit;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        this.btnKysely =  findViewById(R.id.btnKysely);
+        this.btnRaportit = findViewById(R.id.btnRaportit);
+
+
+        btnKysely.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Kysely-activityn (näkymän) avaus
+                Intent myIntent = new Intent(getApplicationContext(),
+                        KyselyActivity.class);
+                startActivity(myIntent);
+            }
+            });
+
+
+        btnRaportit.setOnClickListener(new View.OnClickListener() {
+
+
+                @Override
+                public void onClick (View v) {
+                    // Kysely-activityn (näkymän) avaus
+                    Intent myIntent = new Intent(getApplicationContext(),
+                            KyselyActivity.class);
+                    startActivity(myIntent);
+                }
+        });
+
+
     }
 }
+
+
